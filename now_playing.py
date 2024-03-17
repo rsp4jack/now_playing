c142c648c33992ad36ba9fa0a8dedbc86b8dcbbf
rsp4jack <creepercdn@outlook.com>
2024-03-17 17:12:11 +0800

    REFACTOR #8

--- now_playing.py
+++ now_playing.py
@@ -1,124 +1,77 @@
 #!/usr/bin/env python
 # title             : now_playing.py
 # description       : Now Playing is an OBS script that will update a Text Source
-#                   : with the current song that Media Player are playing. Only for Windows OS
-# author            : Etuldan(Orgin)
-#                   : Creepercdn(Fork)
-# date              : 2020 12 12
+#                   : with the current song that a media player are playing. 
+#                   : Only for Windows now
+# author            : Etuldan
+#                   : rsp4jack
 # version           : 0.2
 # usage             : python now_playing.py
-# dependencies      : - Python 3.6 (https://www.python.org/)
+# dependencies      : - Python 3.7+ (https://www.python.org/)
 #                   :   - pywin32 (https://github.com/mhammond/pywin32/releases)
 #                   : - Windows Vista+
-# notes             : Follow this step for this script to work:
-#                   : Python:
-#                   :   1. Install python (v3.6 and 64 bits, this is important)
-#                   :   2. Install pywin32 (not with pip, but with installer)
-#                   : OBS:
-#                   :   1. Create a GDI+ Text Source with the name of your choice
-#                   :   2. Go to Tools › Scripts
-#                   :   3. Click the "+" button and add this script
-#                   :   5. Set the same source name as the one you just created
-#                   :   6. Check "Enable"
-#                   :   7. Click the "Python Settings" rab
-#                   :   8. Select your python install path
-#                   :
-# python_version    : 3.6+
+# notes             : 
+# python_version    : 3.7+
 # ==============================================================================
 
+def script_description():
+    log.debug("script_description()")
+
+    return \
+        """
+        <h1>Now Playing</h1>
+        <hr/>
+        Display current playing song as text.
+        <br/>
+        Available placeholders:
+        <br/>
+        <code>%artist</code>, <code>%title</code>
+        <br/>
+        <a href="https://github.com/rsp4jack/now_playing">https://github.com/rsp4jack/now_playing</a>
+        <hr/>
+        """
+
+import asyncio
 import ctypes
 import ctypes.wintypes
+import logging
 import site
+import threading
+import time
+from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
+from itertools import chain
 from types import LambdaType
-from typing import AnyStr, Sequence
+from typing import Any, AnyStr, Callable, Sequence
 
 import win32api
 import win32con
 import win32gui
 import win32process
+import winrt.windows.foundation as _
+from winrt.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionManager as SMTCManager
+from winrt.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionMediaProperties as SMTCProperties
 
 import obspython as obs
 
-site.main()
+def timeit(func):
+    async def process(func, *args, **params):
+        if asyncio.iscoroutinefunction(func):
+            return await func(*args, **params)
+        else:
+            return func(*args, **params)
 
-enabled = True
-check_frequency = 1000  # ms
-display_text = '%artist - %title'
-debug_mode = True
+    async def helper(*args, **params):
+        start = time.time()
+        result = await process(func, *args, **params)
+        end = time.time()
 
-source_name = ''
-calcingfunc = {}
+        log.debug(f'{func.__name__} takes {end - start}s')
+        return result
 
-class Capture(object):
-    def __init__(self, id: AnyStr, display_name: AnyStr, state: bool) -> None:
-        self.id = id
-        self.display_name = display_name
-        self.state = state
-    
-    def capture(self, title: AnyStr, process: AnyStr) -> Sequence[str]:
-        """Capture the song info
-
-        Args:
-            title (AnyStr): [description]
-            process (AnyStr): [description]
-
-        Returns:
-            Sequence[str]: 0:Artist, 1:Song
-        """
-        # print((lambda x,y: ((x[0:x.find('-')-1], x[x.find('-')+2:x.rfind('-')-1]) if (y.lower().endswith('vlc.exe') and captures.vlc.state and "-" in x) else (None, None)))(title, process))
-        return calcingfunc.get(self.id, lambda x,y: ('Underdefined', 'ERR'))(title, process)
-
-
-# We defined some factory presets here for you.
-# BEGIN PRESETS DEFINE
-
-# BEGIN SPECIAL CAPTURES
-def foobar2000capture(title: AnyStr, process: AnyStr) -> Sequence[str]:
-    artist = ''
-    song = ''
-    if foobar2000.state and process.lower().endswith('foobar2000.exe'):
-        if ("-" not in title) and (title.find('[foobar2000]') != -1):
-            song = title[:title.rfind(" [foobar2000]")-1]
-        elif "-" in title:
-            artist = title[0:title.find("-")-1]
-            song = title[title.find("]")+2:title.rfind(" [foobar2000]")-1]
-    return (artist, song)
-
-def serato_capture(url: str):
-    # Thanks DachsbauTV for this feature! (#2)
-    # TODO: Add Serato capture (#2)
-    pass
-
-# END SPECIAL CAPTURES
-
-spotify = Capture('spotify', 'Spotify', True)
-calcingfunc['spotify'] = lambda x,y: ((x[0:x.find('-')-1], x[x.find('-')+2:]) if (y.lower().endswith('spotify.exe') and spotify.state and "-" in x) else (None, None))
-vlc = Capture('vlc', "VLC", True)
-calcingfunc['vlc'] = lambda x,y: ((x[0:x.find('-')-1], x[x.find('-')+2:x.rfind('-')-1]) if (y.lower().endswith('vlc.exe') and vlc.state and "-" in x) else (None, None))
-yt_firefox = Capture('yt_firefox', "YouTube for Firefox", True)
-calcingfunc['yt_firefox'] = lambda x,y: ((x[0:x.find('-')-1], x[x.find('-')+2:x.rfind('-')-1]) if (y.lower().endswith('firefox.exe') and yt_firefox.state and "- YouTube" in x) else (None, None))
-yt_chrome = Capture('yt_chrome', 'YouTube for Chrome', True)
-calcingfunc['yt_chrome'] = lambda x,y: ((x[0:x.find('-')-1], x[x.find('-')+2:x.rfind('-')-1]) if (y.lower().endswith('chrome.exe') and yt_chrome.state and "- YouTube" in x) else (None, None))
-
-foobar2000 = Capture('foobar2000', 'Foobar2000', True)
-calcingfunc['foobar2000'] = foobar2000capture
-necloud = Capture('necloud', 'Netease Cloud Music', True)
-calcingfunc['necloud'] = lambda x,y: ((x[x.find("-")+2:], x[0:x.find("-")-1]) if (y.lower().endswith('cloudmusic.exe') and necloud.state and "-" in x) else (None, None))
-aimp = Capture('aimp', 'AIMP', True)
-calcingfunc['aimp'] = lambda x,y: ((x[0:x.find('-')-1], x[x.find('-')+2:]) if (y.lower().endswith('aimp.exe') and aimp.state and "-" in x) else (None, None))
-
-# END PRESETS DEFINE
-
-def debug(*args, sep: str = ' ', end: str = '\n', flush: bool = False) -> None:
-    """A debug info printer
-
-    Args:
-        sep (str, optional): [description]. Defaults to ' '.
-        end (str, optional): [description]. Defaults to '\n'.
-        flush (bool, optional): [description]. Defaults to False.
-    """
-    if debug_mode:
-        print(*args, sep=sep, end=end, flush=flush)
+    return helper
 
 def IsWindowVisibleOnScreen(hwnd):
     def IsWindowCloaked(hwnd):
@@ -129,57 +82,120 @@ def IsWindowVisibleOnScreen(hwnd):
         return cloaked.value
     return ctypes.windll.user32.IsWindowVisible(hwnd) and (not IsWindowCloaked(hwnd))
 
-def callsmtc():
-    # TODO: SMTC.py supports
-    pass
+enabled = False
+check_frequency = 1000  # ms
+display_text = ''
+source_name = ''
+encaptureSet: set[str] = set()
 
+logging.basicConfig(
+    format="[{asctime}] [{threadName}/{levelname}]: [{module}]: {message}",
+    datefmt="%H:%M:%S",
+    style="{",
+    level=logging.DEBUG
+)
+
+log = logging.getLogger(__name__)
+
+class Capture(object):
+    def __init__(self, id: AnyStr, display_name: AnyStr, func: Callable):
+        self._id = id
+        self._display_name = display_name
+        self._func = func
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def display_name(self):
+        return self._display_name
+
+    def __call__(self, *args, **kwargs):
+        return self._func(*args, **kwargs)
+
+def win32TitleCaptureWrapper(process: str):
+    def wrapper(func: Callable):
+        def wrapped() -> list[dict[str, Any]]:
+            def enumHandler(hwnd, result: list[dict[str, Any]]):
+                _, procpid = win32process.GetWindowThreadProcessId(hwnd)
+                try:
+                    if not IsWindowVisibleOnScreen(hwnd):
+                        return
+                    mypyproc = win32api.OpenProcess(
+                        win32con.PROCESS_QUERY_INFORMATION, False, procpid)
+                    image: str = win32process.GetModuleFileNameEx(mypyproc, 0)
+                    title: str = win32gui.GetWindowText(hwnd)
+                    
+                    if image.split('\\')[-1] == process:
+                        result.extend(func(title))
+                except Exception:
+                    log.warning(f'enumHandler error for {process}', exc_info=True)
+                    return
+                return
+            result: list[dict[str, Any]] = []
+            win32gui.EnumWindows(enumHandler, result)
+            return result
+        return wrapped
+    return wrapper
+
+def foobar2000capture(title: str) -> tuple[str, str]:
+    artist = ''
+    song = ''
+    if ("-" not in title) and (title.find('[foobar2000]') != -1):
+        song = title[:title.rfind(" [foobar2000]")-1]
+    elif "-" in title:
+        artist = title[0:title.find("-")-1]
+        song = title[title.find("]")+2:title.rfind(" [foobar2000]")-1]
+    return (artist, song)
+
+@timeit
+async def smtcCaptureAsync() -> list[dict[str, Any]]:
+    manager: SMTCManager = await SMTCManager.request_async()
+    session = manager.get_current_session()
+    if not session:
+        return []
+    properties: SMTCProperties = await session.try_get_media_properties_async()
+    # TODO: more properties
+    return [{'artist': properties.artist, 'title': properties.title}]
+
+def smtcCapture() -> list[dict[str, Any]]:
+    return asyncio.run_coroutine_threadsafe(smtcCaptureAsync(), loop).result(5)
+
+captures: dict[str, Capture] = {
+    'smtc': Capture('smtc', 'SMTC', smtcCapture),
+    'spotify': Capture('spotify', 'Spotify', win32TitleCaptureWrapper('spotify.exe')(lambda x: [{'artist': x[0:x.find('-')-1], 'title': x[x.find('-')+2:]}] if '-' in x else [])),
+    'vlc': Capture('vlc', "VLC", win32TitleCaptureWrapper('vlc.exe')(lambda x: [{'artist': x[0:x.find('-')-1], 'title': x[x.find('-')+2:x.rfind('-')-1]}] if '-' in x else [])),
+    'yt_firefox': Capture('yt_firefox', "YouTube for Firefox", win32TitleCaptureWrapper('firefox.exe')(lambda x: [{'artist': x[0:x.find('-')-1], 'title': x[x.find('-')+2:x.rfind('-')-1]}] if '- YouTube' in x else [])),
+    'yt_chrome': Capture('yt_chrome', 'YouTube for Chrome', win32TitleCaptureWrapper('chrome.exe')(lambda x: [{'artist': x[0:x.find('-')-1], 'title': x[x.find('-')+2:x.rfind('-')-1]}] if '- YouTube' in x else [])),
+    'foobar2000': Capture('foobar2000', 'foobar2000', win32TitleCaptureWrapper('foobar2000.exe')(foobar2000capture)),
+    'necloud': Capture('necloud', 'necloud', win32TitleCaptureWrapper('cloudmusic.exe')(lambda x: [{'artist': x[x.find("-")+2:], 'title': x[0:x.find("-")-1]}] if '-' in x else [])),
+    'aimp': Capture('aimp', 'AIMP', win32TitleCaptureWrapper('aimp.exe')(lambda x: [{'artist': x[0:x.find('-')-1], 'title': x[x.find('-')+2:]}] if '-' in x else [])),
+}
 
 def script_defaults(settings):
-    debug("Calling defaults")
+    log.debug(f"script_defaults({settings!r})")
 
-    obs.obs_data_set_default_bool(settings, "enabled", enabled)
-    obs.obs_data_set_default_int(settings, "check_frequency", check_frequency)
-    obs.obs_data_set_default_string(settings, "display_text", display_text)
-    obs.obs_data_set_default_string(settings, "source_name", source_name)
-    for i in filter(lambda x: not x.startswith('_'), calcingfunc.keys()):
-        obs.obs_data_set_default_bool(settings, globals()[i].id, globals()[i].state)
-
-
-def script_description():
-    debug("Calling description")
-
-    return \
-        """
-        <h1>Now Playing by Creepercdn</h1>
-        <hr/>
-        Display current song as a text on your screen.
-        <br/>
-        Available placeholders:
-        <br/>
-        <code>%artist</code>, <code>%title</code>
-        <br/>
-        GitHub:
-        <a href="https://github.com/Creepercdn/now_playing">Creepercdn/now_playing</a>
-        <hr/>
-        """
-
-
-def script_load(_):
-    debug("[CS] Loaded script.")
-
+    obs.obs_data_set_default_bool(settings, "enabled", True)
+    obs.obs_data_set_default_int(settings, "check_frequency", 1000)
+    obs.obs_data_set_default_string(settings, "display_text", "%artist - %title")
+    obs.obs_data_set_default_string(settings, "source_name", '')
+    for name in captures.keys():
+        obs.obs_data_set_default_bool(settings, name, name == 'smtc')
 
 def script_properties():
-    debug("[CS] Loaded properties.")
+    log.debug("script_properties()")
+    log.info(f'locale: {obs.obs_get_locale()}')
 
     props = obs.obs_properties_create()
     obs.obs_properties_add_bool(props, "enabled", "Enabled")
     obs.obs_properties_add_bool(props, "debug_mode", "Debug Mode")
     obs.obs_properties_add_int(
-        props, "check_frequency", "Check frequency", 150, 10000, 100)
+        props, "check_frequency", "Check frequency", 150, 60000, 100)
     obs.obs_properties_add_text(
         props, "display_text", "Display text", obs.OBS_TEXT_DEFAULT)
-    for i in filter(lambda x: not x.startswith('_'), calcingfunc.keys()):
-        obs.obs_properties_add_bool(props, globals()[i].id, globals()[i].display_name)
+    for name, cap in captures.items():
+        obs.obs_properties_add_bool(props, name, cap.display_name)
 
     p = obs.obs_properties_add_list(
         props, "source_name", "Text source",
@@ -196,18 +212,10 @@ def script_properties():
 
     return props
 
-
 def script_save(settings):
-    debug("[CS] Saved properties.")
+    log.debug(f"script_save({settings!r})")
 
     script_update(settings)
-
-
-def script_unload():
-    debug("[CS] Unloaded script.")
-
-    obs.timer_remove(get_song_info)
-
 
 def script_update(settings):
     global enabled
@@ -215,34 +223,40 @@ def script_update(settings):
     global check_frequency
     global source_name
     global debug_mode
-    debug("[CS] Updated properties.")
+    log.debug(f"script_update({settings!r})")
 
-    if obs.obs_data_get_bool(settings, "enabled"):
-        if not enabled:
-            debug("[CS] Enabled song timer.")
-            enabled = True
-            obs.timer_add(get_song_info, check_frequency)
+    if obs.obs_data_get_bool(settings, "debug_mode"):
+        logging.getLogger().setLevel(logging.DEBUG)
     else:
-        if enabled:
-            debug("[CS] Disabled song timer.")
-            enabled = False
-            obs.timer_remove(get_song_info)
-
-    debug_mode = obs.obs_data_get_bool(settings, "debug_mode")
+        logging.getLogger().setLevel(logging.INFO)
     display_text = obs.obs_data_get_string(settings, "display_text")
     source_name = obs.obs_data_get_string(settings, "source_name")
-    check_frequency = obs.obs_data_get_int(settings, "check_frequency")
-    for i in filter(lambda x: not x.startswith('_'), calcingfunc.keys()):
-        globals()[i].state = obs.obs_data_get_bool(settings, globals()[i].id)
+    new_check_frequency = obs.obs_data_get_int(settings, "check_frequency")
+    encaptureSet.clear()
+    for name in captures.keys():
+        if obs.obs_data_get_bool(settings, name):
+            encaptureSet.add(name)
+    
+    if new_check_frequency != check_frequency and enabled:
+        check_frequency = new_check_frequency
+        obs.timer_remove(onUpdate)
+        obs.timer_add(onUpdate, check_frequency)
+    
+    if obs.obs_data_get_bool(settings, "enabled"):
+        if not enabled:
+            log.debug('Timer started')
+            enabled = True
+            obs.timer_add(onUpdate, check_frequency)
+    else:
+        if enabled:
+            log.debug('Timer killed')
+            enabled = False
+            obs.timer_remove(onUpdate)
 
-
-
-def update_song(artist="", song=""):
-
-    now_playing = ""
-    if(artist != "" or song != ""):
-        now_playing = display_text.replace(
-            '%artist', artist).replace('%title', song)
+def update_song(data: dict[str, Any]):
+    now_playing = display_text
+    for key, value in data.items():
+        now_playing = now_playing.replace(f"%{key}", value)
 
     settings = obs.obs_data_create()
     obs.obs_data_set_string(settings, "text", now_playing)
@@ -250,40 +264,47 @@ def update_song(artist="", song=""):
     obs.obs_source_update(source, settings)
     obs.obs_data_release(settings)
     obs.obs_source_release(source)
-    debug("[CS] Now Playing : " + artist + " / " + song)
+
+    log.debug(f"updated: {now_playing} <- {data}")
+
+tpool = ThreadPoolExecutor(4, 'nowplaying_updateworker')
+loop = asyncio.new_event_loop()
+loop.set_default_executor(tpool)
+loopthread: threading.Thread | None = None
+def start_eventloop():
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+def startevthread():
+    global loopthread
+    if loopthread and loopthread.is_alive():
+        log.warning('loopthread is still alive!!!')
+    loopthread = threading.Thread(target=start_eventloop, name='nowplaying_eventloop', daemon=True)
+    loopthread.start()
+
+def script_load(_):
+    log.info('script_load()')
+    startevthread()
+
+def script_unload():
+    log.info('script_unload()')
+    obs.timer_remove(onUpdate)
+    _ = loop.call_soon(loop.stop)
 
 
-def get_song_info():
-
-    def enumHandler(hwnd, result):
-
-        _, procpid = win32process.GetWindowThreadProcessId(hwnd)
-        try:
-            if not IsWindowVisibleOnScreen(hwnd):
-                return
-            mypyproc = win32api.OpenProcess(
-                win32con.PROCESS_ALL_ACCESS, False, procpid)
-            exe = win32process.GetModuleFileNameEx(mypyproc, 0)
-            title = win32gui.GetWindowText(hwnd)
-            for i in filter(lambda x: not x.startswith('_'), calcingfunc.keys()):
-                res = globals()[i].capture(title, exe)
-                
-                # 0:Arist, 1:Song
-                artist = ''
-                song = ''
-                if res[0]:
-                    artist = res[0]
-                if res[1]:
-                    song = res[1]
-                if any([artist, song]):
-                    result.append([artist, song])
-        except:
+def onUpdate():
+    fut = asyncio.run_coroutine_threadsafe(doUpdate(), loop)
+    def callback(f):
+        if exc := f.exception():
+            log.error('doUpdate error', exc_info=exc)
             return
-        return
+    fut.add_done_callback(callback)
 
-    result = []
-    win32gui.EnumWindows(enumHandler, result)
+async def doUpdate():
+    datalist = await asyncio.gather(*[asyncio.to_thread(captures[name]) for name in encaptureSet ])
+    data: list[dict[str, Any]] = list(chain(*datalist))
+    
+    log.debug(f"doUpdate: {data}")
     try:
-        update_song(result[0][0], result[0][1])
-    except:
-        update_song()
+        update_song(data[0])
+    except Exception:
+        log.error('onUpdate error', exc_info=True)
